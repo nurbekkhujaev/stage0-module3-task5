885213eb1c1abb4f819dbfdab0992ee5c73ce7bb
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-10-27 22:46:06 +0500

    bugs fixed

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        double divided = toBeDivided / 5;
+        double divided = toBeDivided / 5.0;
     }
 
 
